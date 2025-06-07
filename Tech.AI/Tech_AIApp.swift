bbd21190717a0579298b952e95fe16f05e9c4cec
DevTahsin11 <ahmedtahsin487@gmail.com>
2025-06-06 21:24:45 -0400

    Initial Commit

---
+++ Tech.AI/Tech_AIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tech_AIApp.swift
+//  Tech.AI
+//
+//  Created by Tahsin Ahmed  on 6/6/25.
+//
+
+import SwiftUI
+
+@main
+struct Tech_AIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Tech.AITests/Tech_AITests.swift
@@ -0,0 +1,17 @@
+//
+//  Tech_AITests.swift
+//  Tech.AITests
+//
+//  Created by Tahsin Ahmed  on 6/6/25.
+//
+
+import Testing
+@testable import Tech_AI
+
+struct Tech_AITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
